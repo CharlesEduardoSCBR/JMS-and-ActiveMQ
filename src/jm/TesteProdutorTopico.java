a1735ca5445da83b1ae9698b550ad2280d78634e
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-06-20 18:52:49 -0300

    C2.M5.06 - Consumidor e Produtor para o topico

---
+++ src/jm/TesteProdutorTopico.java
@@ -0,0 +1,37 @@
+package jm;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TesteProdutorTopico {
+
+	public static void main(String[] args) throws NamingException, JMSException {
+
+		InitialContext context = new InitialContext();
+
+		ConnectionFactory cf = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection conexao = cf.createConnection();
+
+		conexao.start();
+
+		Session session = conexao.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination topico = (Destination) context.lookup("loja");
+
+		MessageProducer producer = session.createProducer(topico);
+
+		Message message = session.createTextMessage("<pedido><id>123</id></pedido>");
+		producer.send(message);
+
+		session.close();
+		conexao.close();
+		context.close();
+	}
+
+}
